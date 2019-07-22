26da35142ac0b7681e8e3614785c2d64daaa4233
Kanagaraj Kandasamy <Kanagaraj.Kandasamy@xome.com>
2019-07-23 01:37:24 +0530

    adding unit test case.

--- src/RandomBeer/RandomBeerComponent.test.js
+++ src/RandomBeer/RandomBeerComponent.test.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, mount, configure } from 'enzyme';
 import  RandomBeerComponent from  './RandomBeerComponent'
+import sinon from 'sinon'
 
-    const wrapper = shallow(<RandomBeerComponent />);
+configure({adapter: new Adapter()});
+const getRandomBeer = sinon.spy();
+const getNonAlcoholicBeer = sinon.spy();
+const props = {
+    anotherRandomBeer: getRandomBeer,
+    anotherNonAlcoholicBeer: getNonAlcoholicBeer
+}
+const wrapper = shallow(<RandomBeerComponent  {...props}/>);
+const childComponent = wrapper.find('RandomBeerView');
 
-    describe('Auto Suggestion Container', () => {
-        it('Component should load Properly', () => {
-            expect(wrapper).toBeDefined();
-        });
-    });
+describe('RandomBeer Component', () => {
+  it('Component should load Properly', () => {
+      expect(wrapper).toBeDefined();
+  });
+
+  it('Should be able to call anotherRandomBeer callback', () => {
+    const spy = sinon.spy(wrapper.instance(),'getRandomBeer');
+    childComponent.find('button.another-beer').last().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+    spy.restore();
+  });
+});
+
